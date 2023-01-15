df9d83398b9e3040ec549f5d2367f74b895c3b78
Pascoal Bayonne <eddybayonne@gmail.com>
2023-01-15 16:54:49 +0000

    added topic destination name

--- src/main/java/pt/bayonne/sensei/customer/service/CustomerServiceImpl.java
+++ src/main/java/pt/bayonne/sensei/customer/service/CustomerServiceImpl.java
@@ -1,11 +1,9 @@
 package pt.bayonne.sensei.customer.service;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.cloud.stream.function.StreamBridge;
 import org.springframework.stereotype.Service;
 import pt.bayonne.sensei.customer.domain.Customer;
 import pt.bayonne.sensei.customer.domain.EmailAddress;
-import pt.bayonne.sensei.customer.messaging.CustomerMessaging;
 import pt.bayonne.sensei.customer.repository.CustomerRepository;
 import reactor.core.publisher.Sinks;
 
