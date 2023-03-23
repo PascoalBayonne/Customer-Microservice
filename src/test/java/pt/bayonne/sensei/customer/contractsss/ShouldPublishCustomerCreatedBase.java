24d1e3c53461aa79a7f12061891f7ea90e060ba5
Pascoal Bayonne <eddybayonne@gmail.com>
2023-03-23 22:40:14 +0000

    integration test producer side

---
+++ src/test/java/pt/bayonne/sensei/customer/contractsss/ShouldPublishCustomerCreatedBase.java
@@ -0,0 +1,31 @@
+package pt.bayonne.sensei.customer.contractsss;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.contract.verifier.messaging.boot.AutoConfigureMessageVerifier;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import pt.bayonne.sensei.customer.domain.*;
+import pt.bayonne.sensei.customer.service.CustomerService;
+
+import java.time.LocalDate;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
+@AutoConfigureMessageVerifier
+public class ShouldPublishCustomerCreatedBase {
+
+    @Autowired
+    private CustomerService customerService;
+
+
+     void shouldPublishCustomerCreated(){
+        var customer = Customer.create(FirstName.of("Nasir"),
+                LastName.of("Jones"),
+                BirthDate.of(LocalDate.of(1999,8,11)),
+                EmailAddress.of("nasisthegoat@gmail.com"),SSN.create(888888888));
+         Customer customerCreated = customerService.create(customer);
+         Assertions.assertNotNull(customerCreated);
+     }
+}
