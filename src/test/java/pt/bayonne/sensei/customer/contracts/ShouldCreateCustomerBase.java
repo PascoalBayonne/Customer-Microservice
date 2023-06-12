19c6880662b56df8ec02c8c1a1fcbb3d5c390a06
Pascoal Bayonne <eddybayonne@gmail.com>
2023-06-12 21:53:36 +0100

    fix by adding containers

--- src/test/java/pt/bayonne/sensei/customer/contracts/ShouldCreateCustomerBase.java
+++ src/test/java/pt/bayonne/sensei/customer/contracts/ShouldCreateCustomerBase.java
@@ -2,31 +2,21 @@ package pt.bayonne.sensei.customer.contracts;
 
 import io.restassured.module.mockmvc.RestAssuredMockMvc;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.json.AutoConfigureJson;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.cloud.contract.verifier.messaging.boot.AutoConfigureMessageVerifier;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
-import pt.bayonne.sensei.customer.controller.CustomerController;
+import pt.bayonne.sensei.customer.config.IntegrationTestBaseConfig;
 
-@ExtendWith(SpringExtension.class)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
+
 @AutoConfigureMessageVerifier
-@AutoConfigureJson
-public class ShouldCreateCustomerBase {
+public class ShouldCreateCustomerBase extends IntegrationTestBaseConfig {
 
-    @Autowired
-    private CustomerController customerController;
 
     @Autowired
     private WebApplicationContext webApplicationContext;
 
     @BeforeEach
     public void setUp() {
-        var standaloneMockMvcBuilder = MockMvcBuilders.standaloneSetup(customerController);
         RestAssuredMockMvc.webAppContextSetup(webApplicationContext);
     }
 }
--- src/test/java/pt/bayonne/sensei/customer/contracts/ShouldPublishCustomerCreatedBase.java
+++ src/test/java/pt/bayonne/sensei/customer/contracts/ShouldPublishCustomerCreatedBase.java
@@ -1,31 +1,28 @@
 package pt.bayonne.sensei.customer.contracts;
 
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.cloud.contract.verifier.messaging.boot.AutoConfigureMessageVerifier;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
+import pt.bayonne.sensei.customer.config.IntegrationTestBaseConfig;
 import pt.bayonne.sensei.customer.domain.*;
 import pt.bayonne.sensei.customer.service.CustomerService;
 
 import java.time.LocalDate;
 
-@ExtendWith(SpringExtension.class)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
+
 @AutoConfigureMessageVerifier
-public class ShouldPublishCustomerCreatedBase {
+public class ShouldPublishCustomerCreatedBase extends IntegrationTestBaseConfig {
 
     @Autowired
     private CustomerService customerService;
 
 
-     void shouldPublishCustomerCreated(){
+    void shouldPublishCustomerCreated() {
         var customer = Customer.create(FirstName.of("Nasir"),
                 LastName.of("Jones"),
-                BirthDate.of(LocalDate.of(1999,8,11)),
-                EmailAddress.of("nasisthegoat@gmail.com"),SSN.create(888888888));
-         Customer customerCreated = customerService.create(customer);
-         Assertions.assertNotNull(customerCreated);
-     }
+                BirthDate.of(LocalDate.of(1999, 8, 11)),
+                EmailAddress.of("nasisthegoat@gmail.com"), SSN.create(888888888));
+        Customer customerCreated = customerService.create(customer);
+        Assertions.assertNotNull(customerCreated);
+    }
 }
