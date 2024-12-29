040e0e8f0b7ec4fb785af2158e579fd772297247
Pascoal Bayonne <eddybayonne@gmail.com>
2024-12-29 17:26:45 +0000

    Update version of spring boot. From 3.0.x to 3.4.1

--- src/main/java/pt/bayonne/sensei/customer/domain/FirstName.java
+++ src/main/java/pt/bayonne/sensei/customer/domain/FirstName.java
@@ -1,7 +1,5 @@
 package pt.bayonne.sensei.customer.domain;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Embeddable;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -10,18 +8,16 @@ import org.springframework.util.Assert;
 import java.util.Objects;
 
 @Getter
-@Embeddable
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class FirstName {
 
-    @Column(name = "firstName")
     private String value;
 
-    private FirstName(String value){
+    private FirstName(String value) {
         this.value = value;
     }
 
-    public static FirstName of(final String value){
+    public static FirstName of(final String value) {
         Objects.requireNonNull(value, "the value of the first name cannot be null");
         Assert.isTrue(!value.isBlank(), "the first name cannot be empty");
         return new FirstName(value);
